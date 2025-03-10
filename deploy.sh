a8eeefd1023ee2fa9a73d80074a461cc5c665f40
Krzysztof Jeziorny <git@jeziorny.net>
2025-03-10 10:30:13 +0100

    deploy, gunicorn scripts

---
+++ deploy.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+source="$HOME/projects/linkding"
+
+target="$1"
+if [ -z "$target" ] ; then
+	echo "Bitte ein Ziel als Argument angeben."
+	exit 1
+fi
+
+excludes="$source/deploy_excludes"
+if [ ! -f "$excludes" ] ; then
+	echo "Datei nicht gefunden: $excludes"
+	exit 1
+fi
+
+includes="$source/deploy_includes"
+if [ ! -f "$includes" ] ; then
+	echo "Datei nicht gefunden: $includes"
+	exit 1
+fi
+
+target_path="projects/linkding"
+target_port="22"
+# Wenn "true", dann machen wir vor dem tatsächlichen Rsync-Aufruf einen
+# Trockendurchlauf.
+dry_run="false"
+
+case $target in
+	"alphaville")
+		target_host="alphaville.jeziorny.dev"
+		target_user="worker"
+		dry_run="true"
+		;;
+	"kotangens")
+		target_host="kotangens.acat.cc"
+		target_user="kj"
+		dry_run="true"
+		;;
+	"bobbie")
+		target_host="bobbie"
+		target_user="sklave"
+		;;
+	*)
+		target_host=""
+		target_user=""
+esac
+
+if [ -z "$target_host" ] || [ -z "$target_user" ] ; then
+    echo "Unbekanntes Ziel: $target"
+    exit 1
+fi
+
+echo "Ziel: $target_user@$target_host"
+
+if [ "$dry_run" = "true" ] ; then
+    echo
+    echo "Trockendurchlauf: "
+    rsync --dry-run -e "ssh -p $target_port" --recursive --progress \
+		--delete-after \
+		--include-from="$includes" \
+		--exclude-from="$excludes" \
+		"$source"/ "$target_user"@"$target_host":$target_path/
+    echo
+    echo -n "Das war der Trockendurchlauf. Weiter?"
+    read -r inp
+    if [ "$inp" != "y" ] && [ "$inp" != "j" ] && [ "$inp" != "ja" ] && [ "$inp" != "yes" ] ; then
+        echo 'abbrechen…'
+        exit 0
+    fi
+fi
+
+rsync -e "ssh -p $target_port" --recursive --progress \
+	--delete-after \
+	--include-from="$includes" \
+	--exclude-from="$excludes" \
+	"$source"/ "$target_user"@"$target_host":"$target_path"/
+echo "$target OK."
+
+# vim: tw=0:wrap
